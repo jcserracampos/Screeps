520d5aa36e88fb0ed392eb6a7babc2e76abb3b5e
Júlio Campos <jcserracampos@gmail.com>
2020-06-15 22:44:45 -0300

    Fix Drainer to use correct function (withdraw)

--- role.storageDrainer.js
+++ role.storageDrainer.js
@@ -11,7 +11,7 @@ var roleStorageDrainer = {
             });
 
             if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
